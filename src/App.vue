426e3c1cb1d3f1b742786383418abd8d1f174172
Ali Hamza <ali.hamza4010424@gmail.com>
2019-07-31 09:28:15 +0200

    In this you can learn the vuex state management system  complete guide

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,37 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+      <AddTodo />
+      <FilterTodo />
+      <Todos />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Todos from "./components/Todos";
+import AddTodo from "./components/AddTodo";
+import FilterTodo from "./components/FilterTodo";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    Todos,
+    AddTodo,
+    FilterTodo
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  font-family: "Franklin Gothic Medium", "Arial Narrow", Arial, sans-serif;
+  line-height: 1.6;
+  background-color: #e8f7f0;
+}
+.container {
+  max-width: 1200px;
+  margin: auto;
+  padding: 0 2rem;
 }
 </style>
